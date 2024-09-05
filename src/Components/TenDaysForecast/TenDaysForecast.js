8e6a07f3c23ca245b8ff724017c18bea6767fd96
Aldiyar <nesquikyey@gmail.com>
2024-09-06 00:09:40 +0500

    added visibility for components scroll to make it more accessible for use

--- src/Components/TenDaysForecast/TenDaysForecast.js
+++ src/Components/TenDaysForecast/TenDaysForecast.js
@@ -4,68 +4,70 @@ const TenDaysForecast = () => {
     return (
         <div>
             <span className="days__title">10 day forecast</span>
-            <ul className="days__list">
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-                <li>
-                    <button className="list__btn">
-                        <span className="date__text">30/08</span>
-                        <span className="temperature__text">27°</span>
-                    </button>
-                </li>
-            </ul>
+            <div className="days-list__wrapper">
+                <ul className="days__list">
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="list__btn">
+                            <span className="date__text">30/08</span>
+                            <span className="temperature__text">27°</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/Components/dayHoursForecast/DayHoursForecast.js
+++ src/Components/dayHoursForecast/DayHoursForecast.js
@@ -2,64 +2,53 @@ import DayIcon from '../../resources/day-icon.svg'
 import './DayHoursForecast.scss';
 
 const DayHoursForecast = () => {
-    // const scrollContainer = document.querySelector('.hours__list');
-    // scrollContainer.addEventListener('wheel', (event) => {
-    //     event.preventDefault();
-    //     scrollContainer.scrollLeft += event.deltaY; 
-    // });
     return (
         <div>
             <span className="hours__title">24 hour forecast for <strong>30/08</strong></span>
-            <ul className="hours__list">
-                <li className="hours__list-item">
-                    <div className="hours__condition--block">
-                        <img src={DayIcon} alt="Day" className="codition__icon"/>
-                        <span className="condition__text">Sunny</span>
-                    </div>
-                    <h3 className="temperature__text">27°</h3>
-                    <span className="hours">14:00</span>
-                </li>
-                <li className="hours__list-item">
-                    <div className="hours__condition--block">
-                        <img src={DayIcon} alt="Day" className="codition__icon"/>
-                        <span className="condition__text">Sunny</span>
-                    </div>
-                    <h3 className="temperature__text">27°</h3>
-                    <span className="hours">14:00</span>
-                </li>
-                <li className="hours__list-item">
-                    <div className="hours__condition--block">
-                        <img src={DayIcon} alt="Day" className="codition__icon"/>
-                        <span className="condition__text">Sunny</span>
-                    </div>
-                    <h3 className="temperature__text">27°</h3>
-                    <span className="hours">14:00</span>
-                </li>
-                <li className="hours__list-item">
-                    <div className="hours__condition--block">
-                        <img src={DayIcon} alt="Day" className="codition__icon"/>
-                        <span className="condition__text">Sunny</span>
-                    </div>
-                    <h3 className="temperature__text">27°</h3>
-                    <span className="hours">14:00</span>
-                </li>
-                <li className="hours__list-item">
-                    <div className="hours__condition--block">
-                        <img src={DayIcon} alt="Day" className="codition__icon"/>
-                        <span className="condition__text">Sunny</span>
-                    </div>
-                    <h3 className="temperature__text">27°</h3>
-                    <span className="hours">14:00</span>
-                </li>
-                <li className="hours__list-item">
-                    <div className="hours__condition--block">
-                        <img src={DayIcon} alt="Day" className="codition__icon"/>
-                        <span className="condition__text">Sunny</span>
-                    </div>
-                    <h3 className="temperature__text">27°</h3>
-                    <span className="hours">14:00</span>
-                </li>
-            </ul>
+            <div className="hours-list__wrapper">
+                <ul className="hours__list">
+                    <button className="hours__list-item">
+                        <div className="hours__condition--block">
+                            <img src={DayIcon} alt="Day" className="codition__icon"/>
+                            <span className="condition__text">Sunny</span>
+                        </div>
+                        <h3 className="temperature__text">27°</h3>
+                        <span className="hours">14:00</span>
+                    </button>
+                    <button className="hours__list-item">
+                        <div className="hours__condition--block">
+                            <img src={DayIcon} alt="Day" className="codition__icon"/>
+                            <span className="condition__text">Sunny</span>
+                        </div>
+                        <h3 className="temperature__text">27°</h3>
+                        <span className="hours">14:00</span>
+                    </button>
+                    <button className="hours__list-item">
+                        <div className="hours__condition--block">
+                            <img src={DayIcon} alt="Day" className="codition__icon"/>
+                            <span className="condition__text">Sunny</span>
+                        </div>
+                        <h3 className="temperature__text">27°</h3>
+                        <span className="hours">14:00</span>
+                    </button>
+                    <button className="hours__list-item">
+                        <div className="hours__condition--block">
+                            <img src={DayIcon} alt="Day" className="codition__icon"/>
+                            <span className="condition__text">Sunny</span>
+                        </div>
+                        <h3 className="temperature__text">27°</h3>
+                        <span className="hours">14:00</span>
+                    </button>
+                    <button className="hours__list-item">
+                        <div className="hours__condition--block">
+                            <img src={DayIcon} alt="Day" className="codition__icon"/>
+                            <span className="condition__text">Sunny</span>
+                        </div>
+                        <h3 className="temperature__text">27°</h3>
+                        <span className="hours">14:00</span>
+                    </button>
+                </ul>
+            </div>
         </div>
     )
 }
